d64c02060ba3f44d87684ecc113d2b6d7a8457d4
sbx_user1051 <sbx_user1051@169.254.11.45>
2020-02-16 11:15:25 +0000

    Sun Feb 16 11:15:25 UTC 2020

---
+++ src/BeFaster.App.Tests/Solutions/HLO/HelloSolutionTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BeFaster.App.Solutions.HLO;
+using BeFaster.App.Solutions.SUM;
+using NUnit.Framework;
+
+namespace BeFaster.App.Tests.Solutions.SUM
+{
+    [TestFixture]
+    public class HelloSolutionTest
+    {
+
+        [TestCase("FriendName")]
+        public void ResponseShouldContainParam(string param)
+        {
+            Assert.True(HelloSolution.Hello(param).IndexOf(param,StringComparison.CurrentCultureIgnoreCase) > -1);
+        }
+    }
+}
--- src/BeFaster.App/Solutions/HLO/HelloSolution.cs
+++ src/BeFaster.App/Solutions/HLO/HelloSolution.cs
@@ -1,12 +1,19 @@
-﻿using BeFaster.Runner.Exceptions;
+﻿using System.Text;
 
 namespace BeFaster.App.Solutions.HLO
 {
     public static class HelloSolution
     {
+        /// <summary>
+        /// Returns the Hello message with friendName
+        /// </summary>
+        /// <param name="friendName"></param>
+        /// <returns></returns>
         public static string Hello(string friendName)
         {
-            throw new SolutionNotImplementedException();
+            StringBuilder builder = new StringBuilder("Hello ");
+            builder.Append(friendName);
+            return builder.ToString();
         }
     }
 }
